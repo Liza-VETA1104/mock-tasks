7f0ab7d95e7f79843239acbd48df0eaba9c5b096
Liza-VETA1104 <emakovtseva@gmail.com>
2025-12-08 19:40:07 +0400

    Add files via upload

---
+++ CAC.sql
@@ -0,0 +1,7 @@
+with users_count as (select u.campaign_id, count(user_id) as users_from_camp
+from users u 
+group by campaign_id)
+select campaign_name, round(spent/users_from_camp, 0) as CAC
+from campaigns c
+join users_count u on u.campaign_id = c.campaign_id 
+order by CAC desc
