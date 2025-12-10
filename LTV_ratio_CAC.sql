379ea1241398465dc450bef472b6ddf5baad32c7
Liza-VETA1104 <emakovtseva@gmail.com>
2025-12-10 11:09:21 +0400

    Update and rename LTV30_ratio_CAC.sql to LTV_ratio_CAC.sql

--- LTV_ratio_CAC.sql
+++ LTV_ratio_CAC.sql
@@ -4,7 +4,6 @@ with for_ltv as(
 		c.campaign_name
 	from users u 
 	left join orders o on u.user_id = o.user_id
-		and o.order_date between u.registration_date  and u.registration_date + interval '30 days'
 	join campaigns c on c.campaign_id = u.campaign_id
 	where u.campaign_id is not null),
 users_count as (select u.campaign_id, count(user_id) as users_from_camp
@@ -16,11 +15,18 @@ from campaigns c
 join users_count u on u.campaign_id = c.campaign_id 
 order by CAC)	
 select l.campaign_name, 
-	round(sum(amount)/count(distinct l.user_id)/MAX(c.cac), 2) as ltv30_cac_ratio
+	round(sum(amount)/count(distinct l.user_id)/c.cac, 2) as ltv_cac_ratio
 from for_ltv l
 join cac c on c.campaign_name = l.campaign_name
-group by l.campaign_name
+group by l.campaign_name, c.cac 
 order by 2 desc
+
+
+
+
+
+
+
 
 
 
