b077abffc411d6a64fac3837d17a19001a5756d2
Liza-VETA1104 <emakovtseva@gmail.com>
2025-12-10 09:46:24 +0400

    Update CAC.sql

--- CAC.sql
+++ CAC.sql
@@ -1,7 +1,8 @@
+--расчет CAC
 with users_count as (select u.campaign_id, count(user_id) as users_from_camp
 from users u 
-group by campaign_id)
-select campaign_name, round(spent/users_from_camp, 0) as CAC
+group by campaign_id) --в подзапросе общее кол-во пользователей для каждой камании
+select campaign_name, round(spent/users_from_camp, 0) as CAC -- затраты на каждую кампанию/общее кол-во пользователей каждой кампании
 from campaigns c
 join users_count u on u.campaign_id = c.campaign_id 
 order by CAC desc
